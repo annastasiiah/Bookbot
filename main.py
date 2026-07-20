0f75fde0c00d307885e05ee3343e58f53cfa8ae9
annastasiiah <anastasiahorpynyuk@gmail.com>
2026-07-20 21:07:56 +0000

    Adding fucntion that counts letters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,6 +7,8 @@ def get_book_text(filepath):
 def main():
     book = get_book_text("books/frankenstein.txt")
     num_words = count_words(book)
+    char_count = count_characters(book)
     print(f'Found {num_words} total words')
-
+    print(char_count)
+    
 main()  
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def count_words(text):
     lenght = len(text.split())
     return lenght
+
+def count_characters(text):
+    char_count = {}
+    for char in text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
