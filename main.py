b15ab501793b399780dcad485c12dd08e8bc8604
annastasiiah <anastasiahorpynyuk@gmail.com>
2026-07-20 20:44:56 +0000

    Find number of words in file

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def count_words(text):
+    lenght = len(text.split())
+    return lenght
+
 def main():
     book = get_book_text("books/frankenstein.txt")
-    print(book)
+    num_words = count_words(book)
+    print(f'Found {num_words} total words')
 
 main()  
