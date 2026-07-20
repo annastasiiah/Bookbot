fff9bbe19a896fecde08dc0f3c38bf51d9809371
annastasiiah <anastasiahorpynyuk@gmail.com>
2026-07-20 20:16:44 +0000

    Read book text from file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    book = get_book_text("books/frankenstein.txt")
+    print(book)
+
+main()  
